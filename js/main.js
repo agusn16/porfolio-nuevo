1d349e2267c279eee248b55dbc7ea3a778b98643
agustin <agusbenitez157@gmail.com>
2026-05-28 00:27:36 -0300

    Dividi el codigo base en partes y agregue section diferentes en html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,52 @@
-const botonMenu = document.querySelector(".menu-hamburguesa"); // Selecciona el botón del menú hamburguesa
-const menu = document.querySelector(".navegacion"); // Selecciona el menú de navegación
+const menuBtn = document.querySelector(".menu-hamburguesa");
+const nav = document.querySelector(".navegacion");
 
-botonMenu.addEventListener("click", () => { // Agrega un evento de clic al botón del menú
-    botonMenu.classList.toggle("activo"); // Alterna la clase "activo" en el botón del menú para animar las líneas
-    menu.classList.toggle("activo"); // Alterna la clase "activo" en el menú para mostrar u ocultar el menú desplegable
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("activo");
+    nav.classList.toggle("activo");
+});
+
+
+// =========================
+// CERRAR MENÚ AL HACER CLICK
+// =========================
+
+const navLinks = document.querySelectorAll(".navegacion-principal a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("activo");
+        menuBtn.classList.remove("activo");
+    });
+});
+
+
+// =========================
+// LINK ACTIVO SEGÚN SECTION
+// =========================
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("activo-link");
+
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("activo-link");
+        }
+    });
 });
